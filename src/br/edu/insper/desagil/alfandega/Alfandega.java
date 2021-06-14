9ec8599241f8fe568301cf23080c6fb4d8ba63b6
Luiza Valezim Augusto Pinto <62949391+LuizaValezim@users.noreply.github.com>
2021-06-14 16:29:14 -0300

    ItemTarifado também é um Item

--- src/br/edu/insper/desagil/alfandega/Alfandega.java
+++ src/br/edu/insper/desagil/alfandega/Alfandega.java
@@ -5,28 +5,20 @@ import java.util.List;
 
 public class Alfandega {
 	private List<Item> itens;
-	private List<ItemTarifado> itensTarifados;
 
 	public Alfandega() {
 		this.itens = new ArrayList<>();
-		this.itensTarifados = new ArrayList<>();
 	}
 
 	public void declara(Item item) {
 		this.itens.add(item);
 	}
 
-	public void declara(ItemTarifado itemTarifado) {
-		this.itensTarifados.add(itemTarifado);
-	}
 
 	public double getTotalDeclarado() {
 		double total = 0.0;
 		for (Item item : this.itens) {
 			total += item.getRate() * item.getValor();
-		}
-		for (ItemTarifado itemTarifado : this.itensTarifados) {
-			total += itemTarifado.getRate() * itemTarifado.getValor();
 		}
 		return total;
 	}
@@ -34,12 +26,9 @@ public class Alfandega {
 	public double getTotalDevido() {
 		double total = 0.0;
 		for (Item item : this.itens) {
-			// Mesmo em itens sem tarifa, a alfÃ¢ndega cobra
-			// uma taxa de 1% Por quÃª? Porque eles podem.
-			total += item.getRate() * item.getValor() * 0.01;
-		}
-		for (ItemTarifado itemTarifado : this.itensTarifados) {
-			total += itemTarifado.getRate() * itemTarifado.getValor() * itemTarifado.getTarifa();
+			// Mesmo em itens sem tarifa, a alfândega cobra
+			// uma taxa de 1% Por quê? Porque eles podem.
+			total += item.getRate() * item.getValor() * item.getTarifa();
 		}
 		return total;
 	}
